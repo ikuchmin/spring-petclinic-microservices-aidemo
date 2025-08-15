595397fcd02393de8011be014cbc1b99f7726cbc
ikuchmin <ikuchmin@gmail.com>
2025-08-15 10:21:26 +0400

    Add migration/versioning scripts for customer service

---
+++ spring-petclinic-customers-service/src/main/resources/db/migration/schema/mysql/V1__create_customers_tables.sql
@@ -0,0 +1,34 @@
+-- MySQL schema migration for customers service
+
+CREATE TABLE types (
+    id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(80),
+    CONSTRAINT pk_types PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE INDEX idx_types_name ON types (name);
+
+CREATE TABLE owners (
+    id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    address VARCHAR(255),
+    city VARCHAR(80),
+    telephone VARCHAR(20),
+    CONSTRAINT pk_owners PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE INDEX idx_owners_last_name ON owners (last_name);
+
+CREATE TABLE pets (
+    id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    birth_date DATE,
+    type_id INT(4) UNSIGNED NOT NULL,
+    owner_id INT(4) UNSIGNED NOT NULL,
+    CONSTRAINT pk_pets PRIMARY KEY (id),
+    CONSTRAINT fk_pets_on_owner FOREIGN KEY (owner_id) REFERENCES owners (id),
+    CONSTRAINT fk_pets_on_type FOREIGN KEY (type_id) REFERENCES types (id)
+) ENGINE=InnoDB;
+
+CREATE INDEX idx_pets_name ON pets (name);
