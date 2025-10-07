372269d5316504dd75db3c43e2b2dfa786ecf15b
ikuchmin <ikuchmin@gmail.com>
2025-10-07 17:38:38 +0400

    Enable static service discovery in `api-gateway`:
    - Add `static-discovery` profile to Kubernetes configuration.
    - Introduce bootstrap configuration for static discovery with Config Server.
    - Update `application.yml` with static discovery client instances.
    - Replace Apache HTTP Status dependency with direct status code reference in FallbackController.
    - Comment out unused Consul and Kubernetes client dependencies in `pom.xml`.

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/FallbackController.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/FallbackController.java
@@ -1,6 +1,5 @@
 package org.springframework.samples.petclinic.api.boundary.web;
 
-import org.apache.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,7 @@ public class FallbackController {
 
     @PostMapping("/fallback")
     public ResponseEntity<String> fallback() {
-        return ResponseEntity.status(HttpStatus.SC_SERVICE_UNAVAILABLE)
+        return ResponseEntity.status(503)
                 .body("Chat is currently unavailable. Please try again later.");
     }
 }
